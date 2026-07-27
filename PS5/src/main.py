3925e30353adeca197b76c7e688ba941940ddbfe
Quontom <quontom2255@gmail.com>
2026-07-27 15:35:39 +0900

    FEAT: VEX project init, linting connection

---
+++ PS5/src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       영재정보교육                                                       #
+# 	Created:      7/27/2026, 2:13:33 PM                                        #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brainPS5=Brain()
+
+brainPS5.screen.print("Quontom2255") 
