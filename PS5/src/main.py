41b2ab9c6d47ecad9896083c2183ffe6bd424369
Quontom <quontom2255@gmail.com>
2026-08-03 14:32:22 +0900

    FEAT: 베이스  봇 구동, 제어, 추가

--- PS5/src/main.py
+++ PS5/src/main.py
@@ -14,3 +14,18 @@ from vex import *
 brainPS5=Brain()
 
 brainPS5.screen.print("Quontom2255") 
+
+from vex import * 
+
+m_brain = Brain() 
+right_drive_motor = Motor(Ports.PORT6)
+left_drive_motor = Motor(Ports.PORT1)
+controller = Controller() 
+
+def main(): 
+    while True: 
+        right_velocity = controller.axisD.position() 
+        left_velocity = controller.axisA.position() 
+        right_drive_motor.spin(REVERSE, right_velocity, PERCENT)
+        left_drive_motor.spin(FORWARD, left_velocity, PERCENT) 
+main() 
